9c9d3928014b38b92000da5dd3d943e9f8e87ab3
navyFox <lina.666@list.ru>
2021-05-26 13:49:11 +0300

    можно добавить новую задачу

--- src/components/todo-list.vue
+++ src/components/todo-list.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="todo-list">
+    <div class="todo__input">
+      <input class="input" type="text" v-model="title" placeholder="Add new task">
+      <v-button @click.native="addTask"/>
+    </div>
     <div class="todo-list__header">
       <p>Description</p>
       <p>Actions</p>
     </div>
     <div class="todo-list__body">
-      <div class="todo-list__item" v-for="(index,key) in tasks" :key="key" :class="index.completed?`todo-list__item--completed`:''">
+      <div class="todo-list__item" v-for="(index,key) in tasks" :key="key"
+           :class="index.completed?`todo-list__item--completed`:''">
         <p>{{ index.title }}</p>
         <div class="button-group">
           <v-button color="green" icon="check-fill" @click.native="checkTask(index.id)" v-if="!index.completed"/>
@@ -24,7 +29,10 @@ import axios from "axios";
 export default {
   name: "todo-list",
   components: {VButton},
-  data: () => ({tasks: []}),
+  data: () => ({
+    tasks: [],
+    title: '',
+  }),
   async created() {
     const res = await axios.get('https://jsonplaceholder.typicode.com/todos', {params: {_limit: 5}})
     this.tasks = res.data
@@ -34,9 +42,22 @@ export default {
       console.log(id)
       this.tasks = this.tasks.filter(item => item.id !== id)  //[]
     },
-    checkTask(id){
+    checkTask(id) {
       const task = this.tasks.find(item => item.id === id)
-      if (task){task.completed=!task.completed}
+      if (task) {
+        task.completed = !task.completed
+      }
+    },
+    addTask() {
+            const objectTask = {
+        completed: false,
+        id: this.tasks.length + 100,
+        title: this.title,
+        userId: 1
+      }
+      //Добавить объект в массив this.tasks
+      this.tasks.push(objectTask)
+      this.title=''
     }
   }
 }
